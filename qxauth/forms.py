792067328dd440f3dc9bbeb9d09dbae852b15ec9
qianxiao <qianxiao348@gmail.com>
2025-06-24 22:31:27 +0800

    blog_learn2

---
+++ qxauth/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from .models import CaptchaModel
+
+User = get_user_model()  #  使用Django自带的用户类
+
+# 注册表单
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=20, min_length=2, error_messages={
+        'required':'请输入用户名',
+        'max_length':'用户名长度不能超过20',
+        'min_length':'用户名长度不能少于2'
+    })
+    email = forms.EmailField(error_messages={
+        'required':'请输入邮箱',
+        'invalid':'请输入一个正确的邮箱'
+        })
+    captcha = forms.CharField(max_length=6, min_length=6, error_messages={
+        'required':'请输入验证码',
+        'max_length':'验证码长度只能是6',
+        'min_length':'验证码长度只能是6'
+    })
+    password = forms.CharField(max_length=20, min_length=6, error_messages={
+        'required':'请输入密码',
+        'max_length':'密码长度不能超过20',
+        'min_length':'密码长度不能少于6'
+    })
+    
+    # 检验邮箱是否存在数据库
+    def clean_email(self):
+        email = self.cleaned_data.get('email')  # 通过表单验证后提取 email 字段的值。
+        exists = User.objects.filter(email=email).exists()  # 校验用户填写的邮箱是否已存在于数据库
+        if exists:
+            raise forms.ValidationError('邮箱已被注册！')
+        return email
+
+    # 检验验证码
+    def clean_captcha(self):
+        captcha = self.cleaned_data.get('captcha')
+        email = self.cleaned_data.get('email') 
+        
+        captcha_model = CaptchaModel.objects.filter(email=email, captcha=captcha).first()
+        if not captcha_model:
+            raise forms.ValidationError('验证码错误！')
+        captcha_model.delete()  #  删除验证码
+        return captcha
+        
+         
+class LoginForm(forms.Form):
+    email = forms.EmailField(error_messages={
+        'required':'请输入邮箱',
+        'invalid':'请输入一个正确的邮箱'
+        })
+    password = forms.CharField(max_length=20, min_length=6, error_messages={
+        'required':'请输入密码',
+        'max_length':'密码长度不能超过20',
+        'min_length':'密码长度不能少于6'
+    })
+    remeber = forms.IntegerField(required=False)
